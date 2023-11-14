00b9b69ea94bd36545dec0e634e7e484fc2d5059
maram allaghi <writtenbymaram@gmail.com>
2023-11-14 18:02:20 +0300

    fixing heroku postgres issue

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
 
 # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
 
 # 'sqlite:///users.db'
 db = SQLAlchemy(app)

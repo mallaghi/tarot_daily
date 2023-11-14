28ffaabc26a672780b15f745646be6dd672c707b
maram allaghi <writtenbymaram@gmail.com>
2023-11-14 17:15:47 +0300

    switching to postgres

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,8 @@ import requests
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+# 'sqlite:///users.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 app.config['BASE_URL'] = 'https://get-daily-tarot-fbcbb0855e5f.herokuapp.com/'

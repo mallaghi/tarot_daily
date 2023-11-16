feb7e2724b7ef4d769ca0965a6b1e7137e64c055
maram allaghi <writtenbymaram@gmail.com>
2023-11-16 15:21:50 +0300

    made the email formatting better and more engaging

--- app/utils.py
+++ app/utils.py
@@ -23,25 +23,27 @@ def send_daily_tarot_email(user, app):
     gmail_password = app.config['GMAIL_PASSWORD']
 
     random_card = get_random_tarot_card()
-    card_name = random_card.get("name")
+    card_name_raw = random_card.get("name")
+    card_name = " ".join(word.capitalize() for word in card_name_raw.split("_"))
     card_image_url = random_card.get("image")
     card_meaning = random_card.get("meaning")
 
     unsubscribe_link = f"{app.config['BASE_URL']}/unsubscribe/{user.email}"
     unsubscribe_message = f"<p>If you no longer wish to receive daily Tarot cards, you can unsubscribe here: {unsubscribe_link}</p>"
 
-    message_subject = f"{user.name}'s Tarot Card of the Day"
-    message_body = f"<p> Hey {user.name}, here is your daily Tarot card!</p>"
-    message_body += f"<p>Card: {card_name}\n</p>"
-
+    message_subject = f"{user.name}'s Tarot Card of the Day - {card_name}"
+    message_body = f"<p> Hey {user.name},</p>"
+    message_body += f"<p>Get ready for a magical journey! 🌟 Your Tarot card for today is the {card_name}.</p>"
+    message_body += f"<p>🔮 Let's dive into the mystical vibes and uncover the lessons this card has in store for you:</p>"
+    message_body += f"<p>{card_meaning}</p>"
+    message_body += f"<p>Wishing you a day filled with positive energy and insights!</p>"
 
     image_response = requests.get(card_image_url)
     image_data = image_response.content
     image = MIMEImage(image_data, name=f"{card_name}.jpg", _subtype='jpg')
     image.add_header('Content-ID', '<card_image>')
-    message_body += f'<img src="cid:card_image" alt="{card_name}">\n'
 
-    message_body += f"<p>Card Meaning: {card_meaning}</p>"
+    message_body += f'<img src="cid:card_image" alt="{card_name}">\n'
 
     message_body += unsubscribe_message
 
@@ -70,4 +72,3 @@ def send_daily_tarot_email(user, app):
         print(f"SMTP Response Exception: {e}")
     except Exception as e:
         print(f"An error occurred: {e}")
-
